436b954f3d9c80c316c72254a3aa3ffd9b97284a
YarCoik <yaroslavbor_2020@mail.ru>
2026-01-08 12:43:14 +0500

    Final index fix

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  // ВМЕСТО 'my-repo-name' ВПИШИ ИМЯ СВОЕГО РЕПОЗИТОРИЯ КАК НА ГИТХАБЕ
-  base: '/yarcoik-site/', 
+  base: '/', // <--- ВОТ ЭТО САМОЕ ГЛАВНОЕ ДЛЯ РОУТЕРА
 })
