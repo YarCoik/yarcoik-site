e20f39b6e45214ecc3b09ec7290c4b47905c09de
YarCoik <yaroslavbor_2020@mail.ru>
2026-01-08 12:36:26 +0500

    Update router and pages

--- src/main.jsx
+++ src/main.jsx
@@ -1,14 +1,12 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { BrowserRouter } from 'react-router-dom'
 import App from './App.jsx'
 import './index.css'
 
-// 1. ИМПОРТИРУЙ ЭТУ СТРОКУ
-import { BrowserRouter } from 'react-router-dom'
-
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    {/* 2. ОБЕРНИ APP В BROWSERROUTER */}
+    {/* Используем BrowserRouter для красивых ссылок без решетки */}
     <BrowserRouter>
       <App />
     </BrowserRouter>
